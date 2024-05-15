3d75cbbbb59e39a69cae3b84f8dc04a763e7204e
Samah <samahalsulami15@gmail.com>
2024-05-15 04:08:45 +0300

    Add files via upload

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,10 +1,24 @@
 const inputs = document.getElementsByTagName('input');
-const warning_text = document.getElementsByClassName('.small-text');
+const warning_text = document.querySelectorAll('.small-text');
+const btn = document.querySelector('.btn');
 
-for(let i = 0; i < inputs.length; i++){
+for (let i = 0; i < inputs.length; i++) {
 
     // Event listener for input (typing)
     inputs[i].addEventListener('focus', () => {
         inputs[i].style.border = '2px solid var(--Purple)';
     });
 }
+
+btn.addEventListener('click', () => {
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === '') {
+            inputs[i].style.border = '2px solid var(--Light-red)';
+            warning_text[i].textContent = 'This field cannot be empty';
+        }
+        else {
+            inputs[i].style.border = ''; // Reset border
+            warning_text[i].textContent = ''; // Clear warning text
+        }
+    }
+});
