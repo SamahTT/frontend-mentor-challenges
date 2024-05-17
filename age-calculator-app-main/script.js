8ca7c88278873fe2ddf41bb937463c1b1f9a98d9
Samah <samahalsulami15@gmail.com>
2024-05-17 19:19:56 +0300

    Add files via upload

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,6 +1,9 @@
 const inputs = document.getElementsByTagName('input');
 const warning_text = document.querySelectorAll('.small-text');
 const btn = document.querySelector('.btn');
+const year_result = document.querySelector('.y');
+const month_result = document.querySelector('.m');
+const day_result = document.querySelector('.d');
 const date = new Date();
 
 for (let i = 0; i < inputs.length; i++) {
@@ -12,31 +15,41 @@ for (let i = 0; i < inputs.length; i++) {
 }
 
 btn.addEventListener('click', () => {
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].value === '') {
-            inputs[i].style.border = '2px solid var(--Light-red)';
+    check_input(inputs);
+
+    const current_year = date.getFullYear();
+    const current_month = date.getMonth();
+    const current_day = date.getDay();
+    const years = current_year - inputs[2].value;
+    const months = current_month - inputs[1].value;
+    const days = current_day - inputs[0].value; 
+    console.log(years + months + days);
+    if(months){
+        year_result.textContent = years;
+        month_result.textContent = months; 
+        day_result.textContent = days;
+    }
+    //else if(months == 0){
+
+    //}
+});
+
+function check_input(x){
+    for (let i = 0; i < x.length; i++) {
+        if (x[i].value === '') {
+            x[i].style.border = '2px solid var(--Light-red)';
             warning_text[i].textContent = 'This field cannot be empty';
         }
-        else if (!isDigit(inputs[i].value)) {
-            inputs[i].style.border = '2px solid var(--Light-red)';
-            warning_text[i].textContent = 'Invalid value';
-        }
+        //else if (!isDigit(inputs[i].value)) {
+        //    inputs[i].style.border = '2px solid var(--Light-red)';
+        //    warning_text[i].textContent = 'Invalid value';
+        ///}
         else {
             inputs[i].style.border = ''; // Reset border
             warning_text[i].textContent = ''; // Clear warning text
-            //let years = current_year - year_born;
-            //let months = current_month - month_born;
-            //let days = currnet_day - day_born; 
-            //if(months){
-                //
-            //}
-            //else{
-
-            //}
-
         }
     }
-});
+}
 
 function isDigit(value) {
     return /^\d$/.test(value);
